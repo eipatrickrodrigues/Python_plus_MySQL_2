44dc567f9574d775750f014d195b3dcbdd8ea725
Patrick Rodrigues <77553580+eipatrickrodrigues@users.noreply.github.com>
2021-02-16 15:32:53 -0300

    :+1:

--- index.py
+++ index.py
@@ -20,6 +20,28 @@ def do_login():
     user = login.lineEdit.text()
     password = login.lineEdit_2.text()
 
+def verify_duplicity_email():
+
+    # Verificação de duplicidade de cadastro
+    cursor = banco.cursor()
+    query = f'SELECT * FROM usuarios'
+    cursor.execute(query)
+    all_data = cursor.fetchall() # Todos os registros em matriz
+    email = register.lineEdit_5.text()
+    print(f'E-mail digitado: {email}')
+
+    # Isolamento dos e-mail do banco de dados da matriz
+    cont = 0
+    for cont in range (0,len(all_data)):
+        email = register.lineEdit_5.text()
+        email_to_confirm = all_data[cont][4] # O verificador passa por todas as linhas
+        if (email == email_to_confirm):
+            print(f"E-mail para confirmar: {email_to_confirm}")
+            return False
+        else:
+            return True
+        cont = cont + 1
+
 def do_register():
 
     # Cadastrar novo user
@@ -33,36 +55,24 @@ def do_register():
     if password == password_to_confirm:
         # Senhas se confirmam
 
-        # Verificação de duplicidade de cadastro
-        cursor = banco.cursor()
-        query = f'SELECT * FROM usuarios'
-        cursor.execute(query)
-        all_data = cursor.fetchall()
-
-        cont = 0
-        for email_to_confirm in range (0,len(all_data)):
-            email_to_confirm = all_data[cont][4]
-            print(email_to_confirm)
-            cont += 1
-            if email_to_confirm == email:
-                print(f'E-mail repetido: {email_to_confirm}')
-            else:
-                print('Não há e-mails duplicados.')
-             
-
         try:
-            cursor = banco.cursor()
-            query = f"INSERT INTO usuarios (nome, login, senha, email) VALUES ('{name}', '{login_name}', '{password}', '{email}')"
-            cursor.execute(query)
-            banco.commit()
-            register.label_2.setText('Registro efetuado com sucesso!')
+            if ({name} == ' ' or {login_name} == ' ' or {password} == ' ' or {password_to_confirm} == ' ' or {email == ' '}):
 
-            # Limpa os dados
-            name = register.lineEdit.setText('')
-            login_name = register.lineEdit_2.setText('')
-            password = register.lineEdit_3.setText('')
-            password_to_confirm = register.lineEdit_4.setText('')
-            email = register.lineEdit_5.setText('')
+                register.label_2.setText('Favor preencher todos os campos.')
+            else:
+
+                cursor = banco.cursor()
+                query = f"INSERT INTO usuarios (nome, login, senha, email) VALUES ('{name}', '{login_name}', '{password}', '{email}')"
+                cursor.execute(query)
+                banco.commit()
+                register.label_2.setText('Registro efetuado com sucesso!')
+
+                # Limpa os dados
+                name = register.lineEdit.setText('')
+                login_name = register.lineEdit_2.setText('')
+                password = register.lineEdit_3.setText('')
+                password_to_confirm = register.lineEdit_4.setText('')
+                email = register.lineEdit_5.setText('')
 
         except sqlite3.Error as erro:
             print('Erro no cadastro de novos usuários: ',erro)
@@ -81,6 +91,12 @@ def back_to_login():
     register.close()
     login.show()
 
+def do_register_after_verify():
+    if verify_duplicity_email():
+        do_register()
+    else:
+        register.label_2.setText('E-mail já cadastrado.')
+
 
 
 app = QtWidgets.QApplication([])
@@ -92,7 +108,7 @@ register = uic.loadUi('register.ui')
 login.pushButton_2.clicked.connect(show_do_register) # Chama a tela de registro
 #______________________________________
 register.pushButton_2.clicked.connect(back_to_login)
-register.pushButton.clicked.connect(do_register)
+register.pushButton.clicked.connect(do_register_after_verify)
 
 
 # Start do Sistema
